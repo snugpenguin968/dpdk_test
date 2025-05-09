663578f2652b0d20ced661afc0a4707da8befcc9
snugpenguin968 <85849926+snugpenguin968@users.noreply.github.com>
2025-05-09 00:33:16 +0000

    verify successful dpdk installation

---
+++ README.md
@@ -0,0 +1,3 @@
+# dpdk_test
+
+sudo LD_LIBRARY_PATH=/home/aerial/dpdk_test/dpdk/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH ./test_dpdk
---
+++ dpdk_application/test_dpdk.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <rte_eal.h> 
+#include <rte_debug.h>
+#include <rte_version.h> // For getting DPDK version
+
+int main(int argc, char *argv[]) {
+    int ret;
+
+    // 1. Initialize the Environment Abstraction Layer (EAL)
+    ret = rte_eal_init(argc, argv);
+    if (ret < 0) {
+        rte_panic("ERROR: Cannot init EAL\n");
+    }
+
+    // If EAL initialization is successful, print a success message
+    // and the DPDK version.
+    printf("EAL initialized successfully.\n");
+    printf("DPDK Version: %s\n", rte_version());
+
+    // Clean up the EAL
+    // This should be called when the application is exiting.
+    rte_eal_cleanup();
+
+    printf("EAL cleaned up.\n");
+
+    return 0;
+}
